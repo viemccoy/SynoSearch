6ce5d3508645a93776f109f72bc3e04066c922f4
v333 <v@violetcastles.com>
2024-01-10 01:40:03 -0800

    fixed ai!?

--- synosearch/components/airesponse.tsx
+++ synosearch/components/airesponse.tsx
@@ -2,48 +2,30 @@
 
 import React, { useEffect, useState } from 'react';
 
+import { Ai } from '@cloudflare/ai'
+
 type AIResponseProps = {
   prompt: string;
   onResponseChange: (newResponse: string) => void;
 };
 
 const AIResponse: React.FC<AIResponseProps> = ({ prompt, onResponseChange }) => {
-  const [response, setResponse] = useState(null);
+  const ai = new Ai(env.AI);
 
   useEffect(() => {
     const fetchAIResponse = async () => {
-      // Check if window object is defined
-      if (typeof window !== 'undefined') {
-        const response = await fetch('https://nym.synosearch.workers.dev/', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            model: '@cf/meta/llama-2-7b-chat-int8',
-            input: {
-              prompt,
-              stream: true
-            }
-          })
-        });
+      const answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+        prompt,
+        stream: true
+      });
 
-        const data = await response.json();
-        const answer = data.result; // Replace this with the actual path to the answer in the response data
-
-        setResponse(answer);
-        onResponseChange(answer);
-      }
+      onResponseChange(answer);
     };
 
     fetchAIResponse();
   }, [prompt, onResponseChange]);
 
-  return (
-    <div>
-      {response && <p>{response}</p>}
-    </div>
-  );
+  return null;
 };
 
 export default AIResponse;
