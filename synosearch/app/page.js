6701db2092d8968b8310c12d34a77da4351586d1
v333 <v@violetcastles.com>
2024-01-12 01:01:17 -0800

    search functionality in progress!

--- synosearch/app/page.js
+++ synosearch/app/page.js
@@ -1,13 +1,10 @@
-'use client'
-
 import { useState } from "react";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-export default function Home() {
+export default function Page() {
   const [prediction, setPrediction] = useState(null);
   const [error, setError] = useState(null);
 
@@ -28,54 +25,26 @@ export default function Home() {
       return;
     }
     setPrediction(prediction);
-
-    while (
-      prediction.status !== "succeeded" &&
-      prediction.status !== "failed"
-    ) {
-      await sleep(1000);
-      const response = await fetch("/api/predictions/" + prediction.id);
-      prediction = await response.json();
-      if (response.status !== 200) {
-        setError(prediction.detail);
-        return;
-      }
-      console.log({prediction})
-      setPrediction(prediction);
-    }
   };
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Replicate + Next.js</title>
+        <title>SynoSearch</title>
       </Head>
 
-      <p>
-        Dream something with{" "}
-        <a href="https://replicate.com/stability-ai/stable-diffusion">SDXL</a>:
-      </p>
+      <p>Enter your question:</p>
 
       <form className={styles.form} onSubmit={handleSubmit}>
-        <input type="text" name="prompt" placeholder="Enter a prompt to display an image" />
-        <button type="submit">Go!</button>
+        <input type="text" name="prompt" placeholder="Enter a question" />
+        <button type="submit">Search</button>
       </form>
 
       {error && <div>{error}</div>}
 
       {prediction && (
         <div>
-            {prediction.output && (
-              <div className={styles.imageWrapper}>
-              <Image
-                fill
-                src={prediction.output[prediction.output.length - 1]}
-                alt="output"
-                sizes='100vw'
-              />
-              </div>
-            )}
-            <p>status: {prediction.status}</p>
+          <p>Search result: {prediction.output}</p>
         </div>
       )}
     </div>
--- synosearch/pages/api/predictions/[id].js
+++ synosearch/pages/api/predictions/[id].js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
   const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN,
   });
+
   let prediction = await replicate.deployments.predictions.get(
     "viemccoy",
     "nym",
