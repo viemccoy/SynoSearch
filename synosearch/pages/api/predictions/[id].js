343b302b5c9037738a9c426c3d757f04fd3ff7a4
v333 <v@violetcastles.com>
2024-01-12 01:23:27 -0800

    replicate

--- synosearch/pages/api/predictions/[id].js
+++ synosearch/pages/api/predictions/[id].js
@@ -1,14 +1,20 @@
-import Replicate from "replicate";
-
 export default async function handler(req, res) {
-  const replicate = new Replicate({
-    auth: process.env.REPLICATE_API_TOKEN,
-  });
-
-  let prediction = await replicate.deployments.predictions.get(
-    "viemccoy",
-    "nym",
-    req.query.id
+  const response = await fetch(
+    "https://gateway.ai.cloudflare.com/v1/259d9cff4d0f27bf78eb3a6300b4f676/synosearch/replicate/predictions/" + req.query.id,
+    {
+      headers: {
+        Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
   );
+  if (response.status !== 200) {
+    let error = await response.json();
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: error.detail }));
+    return;
+  }
+
+  const prediction = await response.json();
   res.end(JSON.stringify(prediction));
 }
--- synosearch/pages/api/predictions/index.js
+++ synosearch/pages/api/predictions/index.js
@@ -1,20 +1,28 @@
-import Replicate from "replicate";
-
 export default async function handler(req, res) {
-  const replicate = new Replicate({
-    auth: process.env.REPLICATE_API_TOKEN,
+  const response = await fetch("https://gateway.ai.cloudflare.com/v1/259d9cff4d0f27bf78eb3a6300b4f676/synosearch/replicate/predictions", {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // Pinned to a specific version of Stable Diffusion
+      // See https://replicate.com/stability-ai/sdxl
+      version: "2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2",
+
+      // This is the text prompt that will be submitted by a form on the frontend
+      input: { prompt: req.body.prompt },
+    }),
   });
 
-  let prediction = await replicate.deployments.predictions.create(
-    "viemccoy",
-    "nym",
-    {
-      input: {
-        prompt: req.body.prompt,
-      },
-    }
-  );
-  prediction = await replicate.wait(prediction);
+  if (response.status !== 201) {
+    let error = await response.json();
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: error.detail }));
+    return;
+  }
+
+  const prediction = await response.json();
   res.statusCode = 201;
   res.end(JSON.stringify(prediction));
 }
