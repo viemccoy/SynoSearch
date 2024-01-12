d4d5b709a8af26e1c97718c004de0c00ee39f8d8
v333 <v@violetcastles.com>
2024-01-11 18:26:04 -0800

    renamed prediction function

---
+++ functions/predictions/predict.js
@@ -0,0 +1,28 @@
+export default async function handler(req, res) {
+  const response = await fetch("https://gateway.ai.cloudflare.com/v1/259d9cff4d0f27bf78eb3a6300b4f676/synosearch/replicate/predictions", {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // Pinned to a specific version of Stable Diffusion
+      // See https://replicate.com/stability-ai/sdxl
+      version: "2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2",
+
+      // This is the text prompt that will be submitted by a form on the frontend
+      input: { prompt: req.body.prompt },
+    }),
+  });
+
+  if (response.status !== 201) {
+    let error = await response.json();
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: error.detail }));
+    return;
+  }
+
+  const prediction = await response.json();
+  res.statusCode = 201;
+  res.end(JSON.stringify(prediction));
+}
