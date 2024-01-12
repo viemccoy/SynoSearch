7e3f6872a51a471b0b31a1fd1efbabaed6cb0946
v333 <v@violetcastles.com>
2024-01-11 17:36:11 -0800

    fixing functions support

--- synosearch/app/page.js
+++ synosearch/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("/functions", {
+    const response = await fetch("/functions/predictions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -34,7 +34,7 @@ export default function Home() {
       prediction.status !== "failed"
     ) {
       await sleep(1000);
-      const response = await fetch("/functions/" + prediction.id);
+      const response = await fetch("/functions/prediction/" + prediction.id);
       prediction = await response.json();
       if (response.status !== 200) {
         setError(prediction.detail);
