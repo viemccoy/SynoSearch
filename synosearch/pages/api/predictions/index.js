691944e54973fb7951f9c8a6be3cbbaa9f1078f1
v333 <v@violetcastles.com>
2024-01-12 21:01:26 -0800

    changed model

--- synosearch/pages/api/predictions/index.js
+++ synosearch/pages/api/predictions/index.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://api.replicate.com/v1/deployments/viemccoy/nym/predictions", {
+  const response = await fetch("https://api.replicate.com/v1/deployments/viemccoy/nym2/predictions", {
     method: "POST",
     headers: {
       Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
