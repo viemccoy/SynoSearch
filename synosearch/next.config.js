5811d133d47e48a44fe59b9a119fc9f14a3def75
v333 <v@violetcastles.com>
2024-01-12 20:54:17 -0800

    config

--- synosearch/next.config.js
+++ synosearch/next.config.js
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "replicate.com",
-        },
-        {
-          protocol: "https",
-          hostname: "replicate.delivery",
-        },
-      ],
-    },
-  };
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+      {
+        protocol: "https",
+        hostname: "*.replicate.delivery",
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
