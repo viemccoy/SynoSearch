e2ca71e98e1ba42c476cd29f4a53159a8d9d54b8
v333 <v@violetcastles.com>
2024-01-12 21:04:34 -0800

    moving to mixtral

--- synosearch/pages/api/predictions/index.js
+++ synosearch/pages/api/predictions/index.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://api.replicate.com/v1/deployments/viemccoy/nym2/predictions", {
+  const response = await fetch("https://api.replicate.com/v1/models/mistralai/mixtral-8x7b-instruct-v0.1/predictions", {
     method: "POST",
     headers: {
       Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
