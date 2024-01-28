2b35a1386bc1fbe95707c00cc46734c21f765a8c
v333 <v@violetcastles.com>
2024-01-28 00:19:21 -0800

    query fix

--- synosearch/pages/results/[query].js
+++ synosearch/pages/results/[query].js
@@ -3,14 +3,13 @@ import Script from 'next/script'
 
 export default function Results() {
   const router = useRouter()
-  const { query } = router.query
+  const { q } = router.query
 
   return (
     <div>
-      <h1>Results for {query}</h1>
-      <script async src="https://cse.google.com/cse.js?cx=b784b1ad1184941ab">
-      </script>
-      <div class="gcse-searchresults-only"></div>
+      <h1>Results for {q}</h1>
+      <Script src={`https://cse.google.com/cse.js?cx=b784b1ad1184941ab`} strategy="afterInteractive" />
+      <div className="gcse-searchresults-only"></div>
     </div>
   )
 }
