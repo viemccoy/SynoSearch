d9bd1282f8c22310730813b30c12105b0b92a118
v333 <v@violetcastles.com>
2024-03-09 17:58:25 -0800

    post

--- synosearch/pages/api/search/index.js
+++ synosearch/pages/api/search/index.js
@@ -1,19 +1,19 @@
-import Exa from "exa-js"
+const express = require("express");
+const Exa = require("exa-js");
 
+const exa = new Exa(process.env.EXA_API_KEY);
 
-export default async function handler(req, res) {
-    const exa = new Exa(process.env.EXA_API_KEY)
-    if (req.method === 'POST') {
-        const data = req.body;
-        const response = await exa.search(data.query, {
-            numResults: data.numResults,
-            useAutoprompt: data.useAutoprompt,
-        });
+const app = express();
+const port = 8000;
 
-        return res.status(200).json(response);
-    } else {
-        // Handle any other HTTP methods
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
-};
+app.use(express.json()); // This line is necessary to parse JSON request bodies
+
+app.post("/api/search", async (req, res) => {
+  const data = req.body;
+  const exaRes = await exa.search(data.query, {
+    numResults: data.numResults,
+    useAutoprompt: data.useAutoprompt,
+  });
+
+  res.json(exaRes);
+});
