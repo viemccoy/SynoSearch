16d50c883d56933abec7507a9230d24d123a3800
v333 <v@violetcastles.com>
2024-03-09 16:19:04 -0800

    exa!

--- synosearch/pages/api/search/index.js
+++ synosearch/pages/api/search/index.js
@@ -1,12 +1,19 @@
 import Exa from "exa-js"
 
-const exa = new Exa(process.env.EXA_API_KEY)
 
-export const search = async (data) => {
-    const response = await exa.search(data.query, {
-        numResults: data.numResults,
-        useAutoprompt: data.useAutoprompt,
-    });
+export default async function handler(req, res) {
+    const exa = new Exa(process.env.EXA_API_KEY)
+    if (req.method === 'POST') {
+        const data = req.body;
+        const response = await exa.search(data.query, {
+            numResults: data.numResults,
+            useAutoprompt: data.useAutoprompt,
+        });
 
-    return response;
+        return res.status(200).json(response);
+    } else {
+        // Handle any other HTTP methods
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
 };
