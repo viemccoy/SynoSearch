ef3fdbac7894e3bc2b6aa2f5c0110c35798bdbce
v333 <v@violetcastles.com>
2024-03-12 22:53:58 -0700

    moving to vercel

--- synosearch/pages/api/predictions/index.js
+++ synosearch/pages/api/predictions/index.js
@@ -1,55 +1,37 @@
-export default async function handler(req, res) {
-  const { prompt, sysprompt, tokens, temperature, model } = req.body; // Add 'tokens' and 'temperature' here
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
 
-  // Check if the input is too long
-  //if (prompt.length > 230) {
-    //res.statusCode = 400;
-    //res.end(JSON.stringify({ detail: 'Input is too long' }));
-    //return;
-  //}
-  
-  const response = await fetch("https://gateway.ai.cloudflare.com/v1/259d9cff4d0f27bf78eb3a6300b4f676/synosearch", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify([
+// Optional, but recommended: run on the edge runtime.
+// See https://vercel.com/docs/concepts/functions/edge-functions
+export const runtime = 'edge';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export default async function handler(req, res) {
+  const { prompt, sysprompt, tokens, temperature, model } = req.body;
+
+  // Request the OpenAI API for the response based on the prompt
+  const response = await openai.chat.completions.create({
+    model: model,
+    stream: true,
+    messages: [
       {
-        provider: "openai",
-        endpoint: "chat/completions",
-        headers: {
-          "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        query: {
-          model: req.body.model,
-          messages: [
-            {
-              role: "system",
-              content: req.body.sysprompt,
-            },
-            {
-              role: "user",
-              content: req.body.prompt,
-            }
-          ],
-          temperature: req.body.temperature,
-          max_tokens: req.body.tokens,
-        },
+        role: "system",
+        content: sysprompt,
       },
       {
-        "provider": "huggingface",
-        "endpoint": "meta-llama/Llama-2-70b-chat-hf",
-        "headers": {
-          "Authorization": `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
-          "Content-Type": "application/json",
-         },
-        "query": {
-          "input": "Year=2024. Rephrase user search query into an efficient, properly formatted, higher information search query using advanced techniques. You MUST intelligently identify all key terms in the search, and utilize both * wildcards (formatted as “keyterm*” and at minimum one synonym with OR (formatted as “keyterm OR synonym”) for each key term. Never return a full sentence, only a series of key terms, synonyms, and related terms linked by advanced methods in order to generate the most efficient search. Focus on rare or unknown synonyms for depth and breadth of results. Only filter by location if specified. Query follows:" + req.body.prompt,
-        }
-      },
-    ]),
+        role: "user",
+        content: prompt,
+      }
+    ],
+    temperature: temperature,
+    max_tokens: tokens,
   });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
 
   // Check if the response is ok
   if (!response.ok) {
