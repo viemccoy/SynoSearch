e8ea086a376c0e85b92aec9c7dc3f0a5316564cc
v333 <v@violetcastles.com>
2024-03-14 02:01:38 -0700

    help

--- synosearch/pages/api/portkey/index.js
+++ synosearch/pages/api/portkey/index.js
@@ -1,32 +1,22 @@
+import OpenAI from 'openai';
+
 const handler = async (req, res) => {
 
-    const OpenAIApi = require("openai");
+    const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
-    const openai = new OpenAIApi({
-        apiKey: process.env.OPENAI_API_KEY,
-    });
-
-    // Log the request body
     console.log('Request body:', req.body);
 
     try {
-        const response = await openai.createChatCompletion({
+        const response = await openai.chat.completions.create({
             model: req.body.model,
             max_tokens: req.body.tokens,
             temperature: req.body.temperature,
             messages: [{role: "system", content: req.body.sysprompt}, {role: "user", content: req.body.prompt}],
         });
-
-        // Log the response from the OpenAI API call
-        console.log('OpenAI API response:', response);
-
-        const completion = response.data.choices[0].message.content.trim();
-        console.log(completion);
-        res.json(completion);
+        console.log(response);
+        res.json(response);
     } catch (err) {
         console.error('Error fetching from OpenAI API:', err);
         res.status(500).json({ error: 'Failed to fetch from OpenAI API' });
     }
 };
-
-export default handler;
